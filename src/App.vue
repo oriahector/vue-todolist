ce699e1b6ce06bd74fbed5bfcae7cb6ceef83558
Héctor Martínez <hectormartinezoria@Hectors-iMac.local>
2017-09-18 22:36:16 +0200

    revamp 1

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,41 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+html {
+    box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+    box-sizing: inherit;
+}
+
+html,
+body {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    user-select: none;
+    justify-content: center;
+    margin: 0px;
+}
+
+    .toast-success {
+        background-color: #68AD8B;
+        font-family: Avenir;
+    }
+
+    #toast-container {
+        left: 12px;
+    }
+
+    #toast-container>div {
+        box-shadow: none;
+        width: 140px;
+        border-radius: 10px;
+        opacity: 0.5;
+    }
+
+</style>
